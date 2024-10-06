4ac12c2c7cbccc6430e12e7b929123d901a729c5
YastrebovaAnna <ipz221_yaav@student.ztu.edu.ua>
2024-10-06 14:47:45 +0300

    Added state

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,10 +1,22 @@
+import {useState} from 'react'
 import {Square} from "../Square/Square.jsx";
 import './Board.css'
 
 
 // eslint-disable-next-line react/prop-types
 function Board({initialBoard}) {
+
+    const [selectedSquare, setSelectedSquare] = useState(null);
+
     const getSquareColor = (rowIndex, colIndex) => (rowIndex + colIndex) % 2 === 0;
+
+    const handleClick = (rowIndex, colIndex) => {
+        setSelectedSquare({row: rowIndex, col: colIndex});
+    };
+
+    const isSquareSelected = (rowIndex, colIndex) => {
+        return selectedSquare && selectedSquare.row === rowIndex && selectedSquare.col === colIndex;
+    };
 
     return (
         <div className="Board">
@@ -13,10 +25,13 @@ function Board({initialBoard}) {
                 <div key={rowIndex}>
                     {row.map((col, colIndex) => {
                         const isBlack = getSquareColor(rowIndex, colIndex);
+                        const isSelected = isSquareSelected(rowIndex, colIndex);
                         return (
                             <Square
                                 key={`${rowIndex}-${colIndex}`}
                                 isBlack={isBlack}
+                                isSelected={isSelected}
+                                onClick={() => handleClick(rowIndex, colIndex)}
                             />
                         );
                     })}
--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -1,11 +1,15 @@
 import './Square.css'
 
 // eslint-disable-next-line react/prop-types
-function Square({isBlack}) {
-    const squareColorClass = isBlack ? 'black square' : 'white square';
-    return <>
-        <div className={`square ${squareColorClass}`}></div>
-    </>
+function Square({isBlack, isSelected, onClick}) {
+    let squareColorClass = isBlack ? 'black square' : 'white square';
+    if (isSelected)
+        squareColorClass += ' selected';
+
+    return (
+        <div className={`square ${squareColorClass}`} onClick={onClick}>
+        </div>
+    );
 }
 
 export {Square};
